cdef78c669af9f6a95eb3ef92fe2c814e06d37e4
feMoraes0 <fernandomoraes.lopes@gmail.com>
2022-06-22 16:05:44 -0300

    lesson: adds clock counter to form component

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -17,15 +17,15 @@
           class="is-flex is-align-items-center is-justify-content-space-between"
         >
           <section>
-            <strong>00:00:00</strong>
+            <strong>{{ tempoDecorrido }}</strong>
           </section>
-          <button class="button">
+          <button class="button" @click="iniciar">
             <span class="icon">
               <i class="fas fa-play"></i>
             </span>
             <span>play</span>
           </button>
-          <button class="button">
+          <button class="button" @click="finalizar">
             <span class="icon">
               <i class="fas fa-stop"></i>
             </span>
@@ -39,8 +39,29 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+
 export default defineComponent({
   name: "Formulario",
+  data: () => ({
+    tempoEmSegundos: 0,
+    cronometro: 0,
+  }),
+  computed: {
+    tempoDecorrido(): string {
+      return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8);
+    }
+  },
+  methods: {
+    iniciar() {
+      this.cronometro = setInterval(() => {
+        this.tempoEmSegundos += 1;
+        console.log('incrementando');
+      }, 1000);
+    },
+    finalizar() {
+      clearInterval(this.cronometro);
+    },
+  },
 });
 </script>
 
